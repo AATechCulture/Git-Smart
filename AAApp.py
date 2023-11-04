4d7de44c741b17e0f57f8b206e17c6f13a6b9bff
Christian Hall <chall2541@tuskegee.edu>
2023-11-04 16:02:48 -0500

    switchable screens

--- AAApp.py
+++ AAApp.py
@@ -1,9 +1,13 @@
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.lang import Builder
 from kivymd.app import MDApp
-from kivymd.uix.screen import Screen
 from kivymd.uix.button import MDRectangleFlatButton
+from kivy.uix.screenmanager import ScreenManager, Screen
+import cv2
+from PIL import Image
+from pytesseract import pytesseract
 
 #Change size using size_hint
 #Change the color of the button using background_color
@@ -11,21 +15,41 @@ from kivymd.uix.button import MDRectangleFlatButton
 #change position with pos (first number is your x coordinate, the last number is your y coordinate
 #bottom left corner is (0,0)
 #use bind to make button perform function
+class MainScreen(Screen):
+    pass
+
+class MapScreen(Screen):
+    pass
+
+class TranslateScreen(Screen):
+    pass
+
+class ChecklistScreen(Screen):
+    pass
+
 class AAApp(MDApp):
     def build(self):
-        screen = Screen()
+        """screen = Screen()
         self.theme_cls.theme_style = "Dark"
 
         mapBtn = MDRectangleFlatButton(text = "Map",
                                     pos_hint={"center_x":0.3, "center_y":0.3})
         translateBtn = MDRectangleFlatButton(text = "Translate",
-                                    pos_hint={"center_x":0.5, "center_y":0.3})
+                                    pos_hint={"center_x":0.5, "center_y":0.3},
+                                             on_release=self.translateImg)
         checklistBtn = MDRectangleFlatButton(text = "Checklist",
                                              pos_hint={"center_x":0.7, "center_y":0.3})
         screen.add_widget(mapBtn)
         screen.add_widget(translateBtn)
         screen.add_widget(checklistBtn)
-        return screen
+        return screen"""
+        Builder.load_file("screens_layout.kv")
+        sm = ScreenManager()
+
+        sm.add_widget(MainScreen(name = "MainScreen"))
+        sm.add_widget(TranslateScreen(name = "TranslateScreen"))
+
+        return sm
 
 if __name__ == '__main__':
     AAApp().run()
